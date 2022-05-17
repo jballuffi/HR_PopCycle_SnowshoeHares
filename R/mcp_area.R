67338fdc08ba1a7e1f97f5135a8096122685769b
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-17 17:08:08 -0600

    fix lapply issue

--- R/mcp_area.R
+++ R/mcp_area.R
@@ -1,18 +1,13 @@
-
-
 #function that calculates mcp area from a data.table or data.frame
 #Arguments: x = x coord; y = y coord; col1-col3 = additional data to include
 # utmzone = the utm zone; df = the data.table
 
-mcp_area <- function(x, y, col1, col2, col3, utmzone, dt){
+mcp_area <- function(dt, x, y, extra_cols, utmzone) {
   #first convert to a spatial points data frame
-  spdf <- SpatialPointsDataFrame(dt[, .(x, y)],
-                                 data = dt[, .(col1, col2, col3)],
+  spdf <- SpatialPointsDataFrame(dt[, .SD, .SDcols = c(x, y)],
+                                 data = dt[, .SD, .SDcols = extra_cols],
                                  proj4string = CRS(utmzone))
   
   #calculate mcp size
-  size <- as.character(mcp.area(spdf, percent = 95, plotit = FALSE))
-  
-  #return mcp size
-  return(size)
+  return(area = mcp.area(spdf, percent = 95, plotit = FALSE))
 }
