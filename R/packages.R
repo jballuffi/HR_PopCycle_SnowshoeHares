f00676a8623b4596b46fa3e0f1e57a9b092de718
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-17 09:26:17 -0600

    script to grab all packages

---
+++ R/packages.R
@@ -0,0 +1,13 @@
+# Packages 
+
+library(targets)
+library(tarchetypes)
+library(qs)
+
+library(sp)
+library(tidyverse)
+library(zoo)
+library(rgdal)
+library(adehabitatHR)
+library(lubridate)
+library(data.table)
