9d0c14588ccc87fc86659be7cd6ad58dc9a2309d
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-17 17:10:21 -0600

    no filter for now

--- R/list_gps_files.R
+++ R/list_gps_files.R
@@ -1,5 +1,5 @@
 list_gps_files <- function(path) {
   all_paths <- dir(path, pattern = '*.csv', recursive = TRUE, full.names = TRUE)
   
-  grep('XYZ|error', all_paths, value = TRUE)
+  # grep('XYZ|error', all_paths, value = TRUE)
 }
