9fea3c39e8bf7da888588c6693922fd33e495c49
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-18 16:04:08 -0600

    remove onebun

--- R/variables.R
+++ R/variables.R
@@ -1,9 +1,3 @@
 # Variables ---------------------------------------------------------------
 utm7N <- '+proj=utm +zone=7 ellps=WGS84'
 
-
-
-# Prep for test scripts ---------------------------------------------------
-
-#grab just one bunny year
-onebun <- gps[ID == "22130" & winter == "2016-2017"]
