2cc871a83e554077f8ae4501e9ffbb23b0514f09
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-18 15:38:41 -0600

    add onebun to variables

--- R/variables.R
+++ R/variables.R
@@ -1,3 +1,9 @@
 # Variables ---------------------------------------------------------------
 utm7N <- '+proj=utm +zone=7 ellps=WGS84'
 
+
+
+# Prep for test scripts ---------------------------------------------------
+
+#grab just one bunny year
+onebun <- gps[ID == "22130" & winter == "2016-2017"]
