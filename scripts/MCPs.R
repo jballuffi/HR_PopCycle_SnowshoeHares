ab934577f3dcc0eba1c107fe0f86e9112ea294aa
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-18 11:25:48 -0600

    create function to calcuate home range size at different sampling efforts

--- scripts/MCPs.R
+++ scripts/MCPs.R
@@ -41,45 +41,35 @@ gps[, date := tstrsplit(datetime, " ", keep = 1)][, date := mdy(date)]
 
 onebun <- gps[ID == "22130" & year == "2015-2016"]
 
-onebun[, diffday := date - min(date)]
 
-mcp_area(x= 'x.utm', y ='y.utm', 
-        extra_cols = c('ID', 'sex', 'grid'), 
-        utmzone = utm7N, 
-        dt = t1)
-
-buns <- list(
-t8 = onebun[diffday < 8],
-t14 = onebun[diffday < 15],
-t21 = onebun[diffday < 22],
-t28 = onebun[diffday < 29],
-t35 = onebun[diffday < 36])
-
-
-
-lapply(c(8, 15, 22), function(APPLES) {
-  mcp_area(onebun[diffday < APPLES], x = 'x.utm', 'y.utm', 'year', utm7N)
-})
-
-bunintervals <- rbindlist(buns, fill = TRUE,  use.names = TRUE)
-
-
-
-test <- function(bunyear){
+asymptote <- function(subdt){
   
   #calculate the difference between a gps fix date and the first date of the bunny year
-  bunyear[, diffday := date - min(date)]
+  subdt[, diffday := date - min(date)]
   
-
+  effort <- c(8, 15, 22, 29, 36)
   
+  hrs <- lapply(effort, function(n) {
+    mcp_area(subdt[diffday < n], x = 'x.utm', 'y.utm', 'year', utm7N)
+  })
+  
+  bunintervals <- rbindlist(hrs, fill = TRUE,  use.names = TRUE)
+  
+  names(bunintervals) <-"area"
+  bunintervals[, ID := unique(onebun$ID)]
+  bunintervals[, year := unique(onebun$year)]
+  bunintervals[, effortday := effort]
+  
+  return(bunintervals)
   
 }
 
+output <- asymptote(subdt = onebun)
 
 
+output <- list(gps[, asymptote(subdt = onebun), by = c("ID", "year")])
 
-
-
+hreffort <- rbindlist(output, fill = TRUE, use.names = TRUE)
 
 #why do we get a warning when we add other columns (grid, sex, etc), but 
 #it doesn't work when we only use ID?
