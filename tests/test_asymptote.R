81f1c89166f078de2807cb480fb671c6996df932
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-18 17:19:53 -0600

    fix test lines

--- tests/test_asymptote.R
+++ tests/test_asymptote.R
@@ -16,12 +16,15 @@ gps <- fread(sample(files, 1))
 # Subset data -------------------------------------------------------------
 
 onebun <- gps[ID == sample(ID, 1) & date < as.Date()]
-twobun <- rbindlist(lapply(sample(files, 2), fread))
+
 goodbun <- gps[ID == "22130" & winter == "2016-2017"]
 
+morebun <- gps[ID %in% sample(unique(gps$ID), 5, replace = FALSE)] 
 
 
 # Tests -------------------------------------------------------------------
 
 
 out1 <- area_asym(onebun)
+
+outmore <- morebun[, area_asym(DT = .SD), by = c("ID", "winter")]
