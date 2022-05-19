e905d08c835d1040f742b79df16266d8cb30b2a4
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-18 22:53:03 -0600

    add step that removes undersampled asympote points

--- scripts/HR_area_asymptote.R
+++ scripts/HR_area_asymptote.R
@@ -15,24 +15,26 @@ gps <- readRDS("Data/all_gps.rds")
 #grab 30 bunnies randomly
 randbuns <- gps[ID %in% sample(unique(gps$ID), 30, replace = FALSE)] 
 
-#check sample sizes for each ind-year category
+#check sample sizes for each individual-year category
 randbuns[, .N, by = .(ID, winter)]
 
 #run the area_asym function on the sample of hares by ID and by winter
 asym_data <- randbuns[, area_asym(DT = .SD), by = c("ID", "winter")]
 
+#remove rows where the number of collaring days didn't reach the weekly sampling interval
+asym_data <- asym_data[!maxdiffday < daycount]
 
-
-# Look at data ------------------------------------------------------------
-
-
-asym_data[, mean(area), by = ID]
-
-
+#create an ID-winter column
 asym_data[, IDwinter := paste0(ID, " ", winter)]
 
 
 
+# Look at data ------------------------------------------------------------
+
+#calculate mean home range
+asym_data[, mean(area), by = ID]
+
+#take means and sd for each day count, summarising results from all individuals
 asym_means <- asym_data[, .(mean(area), sd(area)), by = daycount]
 names(asym_means) <- c("daycount", "mean", "sd")
 
