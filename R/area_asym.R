8769a8d77ab6536bb97d1f017e85be031e163ff7
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-20 13:44:31 -0600

    increase sampling rate for asymptote

--- R/area_asym.R
+++ R/area_asym.R
@@ -5,7 +5,7 @@
 area_asym <- function(DT){
   
   #declare sample effort categories (count of days)
-  effort <- c(8, 15, 22, 29, 36)
+  effort <- c(5, 9, 13, 17, 21, 25, 29, 33, 37)
   
   #apply the mcp_area function to calculate area of home range to list of sample efforts
   hrs <- lapply(effort, function(n) {
