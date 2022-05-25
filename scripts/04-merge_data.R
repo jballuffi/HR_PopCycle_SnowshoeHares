01fd94c7473183aee2e5704173a2c86b7028828b
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-25 10:47:53 -0600

    make ID factor

--- scripts/04-merge_data.R
+++ scripts/04-merge_data.R
@@ -1,5 +1,8 @@
 
 
+# prep and import data ----------------------------------------------------
+
+
 #source the R folder to load any packages and functions
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
@@ -15,21 +18,30 @@ ldensity <- fread("data/Lynx Density_simple.csv")
 #import trapping data
 trapping <- fread("data/Trapping_data_all_records.csv")
 
-#grab all unique individuals
+
+
+# clean trapping data -----------------------------------------------------
+
+trapping[, ID := as.factor(Eartag)]
+
+#grab all unique individuals from home range calculations
 inds <- unique(areas$ID)
 
-#subset the trapping data.table to only include these inds
-t2 <- trapping[Eartag %in% inds]
+#subset the trapping data.table to only include these individuals
+t2 <- trapping[ID %in% inds]
 
-str(t2)
+#turn dateCap column into a date with lubridate function
 t2[, date := dmy(dateCap)]
 
 #categorize gps fixes into winters
+#should prob turn this into a function
 t2[date > "2015-10-31" & date < "2016-04-01", winter := "2015-2016"]
 t2[date > "2016-10-31" & date < "2017-04-01", winter := "2016-2017"]
 t2[date > "2017-10-31" & date < "2018-04-01", winter := "2017-2018"]
 t2[date > "2018-10-31" & date < "2019-04-01", winter := "2018-2019"]
 
+#remove anything that doesn't fall into winter
+t2<- t2[!is.na(winter)]
 
 #average mass for early winter season (November and December)
 
