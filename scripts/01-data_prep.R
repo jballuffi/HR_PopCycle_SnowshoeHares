dde9b1833880a42bedb44e9cb5e3972b377ffbc1
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-05-26 17:09:36 -0600

    add in burst and other winter years

--- scripts/01-data_prep.R
+++ scripts/01-data_prep.R
@@ -7,30 +7,33 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 # collect files  -----------------------------------------------------------
 
-#create file path to cleaned gps data
+#create file path to cleaned gps data from the prepare locs project
 files <- dir("../prepare-locs-kluane-hares/output/", full.names = TRUE)
 
 
-#lapply a little function to fread the list of files
+#fread the list of files using lapply
 ls.files<-lapply(files, fread)
 
 #rbind the list of files
 gps <- rbindlist(ls.files, fill = TRUE, use.names = TRUE)
 
 
-# Reconstructure data -----------------------------------------------------
+# Reconstruct data -----------------------------------------------------
+
+#overwrite id column with animal
+gps[, id := animal]
 
 #categorize gps fixes into winters
 gps[idate > "2015-10-31" & idate < "2016-04-01", winter := "2015-2016"]
 gps[idate > "2016-10-31" & idate < "2017-04-01", winter := "2016-2017"]
 gps[idate > "2017-10-31" & idate < "2018-04-01", winter := "2017-2018"]
 gps[idate > "2018-10-31" & idate < "2019-04-01", winter := "2018-2019"]
+gps[idate > "2019-10-31" & idate < "2020-04-01", winter := "2019-2020"]
+gps[idate > "2020-10-31" & idate < "2021-04-01", winter := "2020-2021"]
+gps[idate > "2021-10-31" & idate < "2022-04-01", winter := "2021-2022"]
 
 #remove anything that doesn't fall into winter
 gps <- gps[!is.na(winter)]
-
-#remove anything from January
-gps <- gps[mnth != 1]
 
 #create early winter and late winter categories
 #fixes in nov and dec are "early"
@@ -38,18 +41,18 @@ gps <- gps[mnth != 1]
 gps[mnth == 11| mnth == 12, season := "early"]
 gps[mnth == 2| mnth == 3, season := "late"]
 
-
 #variables that we are splitting calculations by
-splitby <- c("id", "winter", "season")
+splitburst <- c("id", "winter", "burst")
+splitseason <- c("id", "winter", "season")
 
-#calculate the difference in days from first day of sampling in winter
-gps[, diffday := idate - min(idate), by = splitby]
+#calculate the difference in days from first day of a burst
+gps[, diffday := idate - min(idate), by = splitburst]
 
-#calculate the total sample range per bunny-season (range of first fix to last fix)
-gps[, samplerange := max(diffday), by = splitby]
+#calculate the total sample range per bunny-burst
+gps[, burstlength := max(diffday), by = splitburst]
 
-#calculate number of unique dates per bunny-season
-gps[, uniquedays := uniqueN(idate), by = splitby]
+#calculate the total sample range per bunny-season
+gps[, seasonlength := max(idate - min(idate)), by = splitseason]
 
 #calculate the difference between days of fix, in order of datetime
 setorder(gps, datetime)
