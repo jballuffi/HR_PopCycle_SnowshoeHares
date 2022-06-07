1ee7d39d5a2745c041dd803c6123929e1e14105f
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-07 16:19:20 -0600

    prep and merge density data with body mass and home range areas

--- scripts/04-merge_data.R
+++ scripts/04-merge_data.R
@@ -16,5 +16,58 @@ hdensity <- fread("data/Hare_density_simple.csv")
 ldensity <- fread("data/Lynx Density_simple.csv")
 
 #import weight results
+weights <- readRDS("output/results/bodymass.rds")
 
 
+# clean hare density ------------------------------------------------------
+
+
+hdensity[, season := tstrsplit(Season_year, " ", keep = 1)]
+hdensity[, year := tstrsplit(Season_year, " ", keep = 2)]
+
+hdensity[, year := as.integer(year)]
+
+hdensity[season == "Fall", season := "early"]
+hdensity[season == "Spring", season := "late"]
+
+hdensity <- hdensity[year > 2010]
+
+hdensity[season == "early", winter := paste(year, "-", year+1)]
+hdensity[season == "late", winter := paste(year-1, "-", year)]
+
+hdensity[, winter := gsub(" ", "", winter)]
+
+hdensity[, Season_year := NULL]
+
+setnames(hdensity, "Density", "haredensity")
+
+hdensity[, haredensity := haredensity*10000]
+
+# merge densities ---------------------------------------------------------
+
+#rename lynx data
+names(ldensity) <- c("winter", "ltracks", "ltrack_se", "ltrack_lower", "ltrack_upper", "lynxdensity")
+#subset just two columns of interest
+lynx <- ldensity[, .(winter, ltracks, lynxdensity)]
+
+
+densities <- merge(hdensity, ldensity, by = "winter", all.x = TRUE)
+
+densities[, ppratio := lynxdensity/haredensity]
+
+plot(densities$ldensity ~ densities$hdensity)
+
+
+
+# merge all data ----------------------------------------------------------
+
+areas[, id := as.factor(id)]
+
+#merge weights with area
+area.weight <- merge(areas, weights, by = c("id", "winter", "season"), all.x = TRUE)
+
+#merge in densities
+area.weight.densities <- merge(area.weight, densities, by = c("winter", "season"), all.x = TRUE)
+
+#save merged data
+saveRDS(area.weight.densities, "output/results/compileddata.rds")
