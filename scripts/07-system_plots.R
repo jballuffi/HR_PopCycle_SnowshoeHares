ac1376178114a068b1cfdc71940e6dc31ecfd55c
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-08 10:09:35 -0600

    script for figures that describe the system

---
+++ scripts/07-system_plots.R
@@ -0,0 +1,43 @@
+
+#source the R folder to load any packages and functions
+lapply(dir('R', '*.R', full.names = TRUE), source)
+
+
+DT <- readRDS("output/results/compileddata.rds")
+
+DT[is.na(mass), mass := mean(mass)]
+
+ppratio <- DT[, mean(ppratio), by = winter]
+
+ggplot(ppratio)+
+  geom_line(aes(y = V1, x = winter, by = winter))
+
+ggplot(DT)+
+  geom_point(aes(y = ppratio, x = winter, color = season))
+
+
+summary(lm(DT$HRninety ~ DT$haredensity*DT$lynxdensity + DT$season + DT$mass))
+
+
+plot(DT$HRninety/DT$mass ~ DT$ppratio)
+
+
+
+ggplot(DT)+
+  geom_point(aes(y = HRninety, x = lynxdensity))
+
+ggplot(DT)+
+  geom_point(aes(y = HRninety, x = haredensity))
+
+ggplot(DT)+
+  geom_boxplot(aes(y = HRninety, x = season))
+
+ggplot(DT)+
+  geom_point(aes(y = HRninety, x = ppratio, color = winter, shape = season), size = 2)+
+  theme_minimal()
+
+ggplot(DT)+
+  geom_point(aes(y = ppratio, x = lynxdensity))
+
+ggplot(DT)+
+  geom_point(aes(y = ppratio, x = haredensity))
