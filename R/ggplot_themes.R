0b465f41311929ff73ddf6a3dcbe0bd8d0023b95
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-08 15:32:37 -0600

    create a ggplot theme

---
+++ R/ggplot_themes.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+theme_densities <- theme(axis.title = element_text(size=12),
+                         axis.text.y = element_text(size=10),
+                         axis.text.x = element_text(size = 9),
+                         legend.position = "top",
+                         legend.key = element_blank(),
+                         panel.background = element_blank(),
+                         axis.line.x.top = element_blank(),
+                         axis.line.y.right = element_blank(),
+                         axis.line.x.bottom = element_line(size=.5),
+                         axis.line.y.left = element_line(size=.5),
+                         panel.border = element_blank(),
+                         panel.grid.major = element_line(size = 0.5, color = "grey90")
+                          )
