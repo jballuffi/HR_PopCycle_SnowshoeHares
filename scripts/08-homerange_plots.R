9ca9de3615cbda491ffc04221864cb6ff7030f0c
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-15 11:03:26 -0600

    add script for home range size figure

---
+++ scripts/08-homerange_plots.R
@@ -0,0 +1,31 @@
+#source the R folder to load any packages and functions
+lapply(dir('R', '*.R', full.names = TRUE), source)
+
+#read in data
+DT <- readRDS("output/results/compileddata.rds")
+DT[, Food := as.factor(Food)]
+
+(byyear <- 
+  ggplot(DT)+
+  geom_boxplot(aes(x = winter, y = HRninety), outlier.shape = NA)+
+  geom_jitter(aes(x = winter, y = HRninety), alpha = .5, width = .3)+
+  labs(y = "90% MCP area (ha)", x = "Winter")+
+  theme_boxplots)
+
+
+(byhdensity <- 
+  ggplot(DT)+
+  geom_point(aes(x = haredensity/10000, y = HRninety))+
+  labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
+  theme_densities)
+
+(byppratio <- 
+  ggplot(DT)+
+  geom_point(aes(x = ppratio, y = HRninety))+
+  labs(y = "90% MCP area (ha)", x = "Lynx:Hare Ratio")+
+  theme_densities)
+
+(hrsize <- ggarrange(byyear, byhdensity, byppratio, ncol = 1, nrow = 3))
+
+
+ggsave("output/figures/hrsize.jpeg", hrsize, width = 6, height = 8, units = "in")
