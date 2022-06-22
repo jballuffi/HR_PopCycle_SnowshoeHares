dbb038954907f9335c6b762e193924f083bbbe76
Liam-Horne <lhorne@ualberta.ca>
2022-06-22 12:20:11 -0600

    more cleaning on utms coords & plotting

--- scripts/03-calc_HRarea.R
+++ scripts/03-calc_HRarea.R
@@ -11,9 +11,12 @@ gps <- gps[burstlength >= 10]
 
 gps <- gps[!is.na(burst)]
 
-#remove unlikely fixes based on small UTM easting
-gps <- gps[x_proj > 300000]
-#check why one mean y.proj became 0???
+#remove outlier fixes based on distribution of easting and northings
+#eastings
+gps <- gps[x_proj > 300000 & x_proj < 342000] #removed 10
+#northings
+gps <- gps[y_proj > 6750000 & y_proj < 6770000] #removed 2 more
+#check why one mean y_lat became 0 when just y_lat was non-zero???
 
 
 #MCP size at 90% and 50%, keep id, winter, season, and grid
@@ -50,6 +53,12 @@ b25618<-gps[id == "25618"]
 b26316<-gps[ id == "26316"]
 b26342<-gps[id == "26342"]
 
+#new id appears after removing xproj<300000?
+b26383<-gps[id == "26383"] 
+ggplot(b26383, aes(x=x_proj, y=y_proj)) +
+  geom_point(aes(size=0.1, colour= datetime)) #still other smaller outliers - hopefully would get caught by out-and-back cleaning later
+
+
 ggplot(b23698, aes(x=x_proj, y=y_proj)) +
      geom_point(aes(size=0.1, colour= datetime))#still other outliers - hopefully would get caught by out-and-back cleaning later
 ggplot(b25618, aes(x=x_proj, y=y_proj)) +
@@ -64,3 +73,20 @@ w20.21<-gps[winter == "2020-2021"]
 
 ggplot(w20.21, aes(x=x_proj, y=y_proj)) +
   geom_point(aes(size=0.1, colour= datetime, shape= as.factor(id))) 
+
+
+#testing if can do plot by all individs to inspect
+gps<-as.data.frame(gps)
+
+p<-ggplot(gps, aes(x=x_proj, y=y_proj)) +
+  geom_point(aes(size=0.1, colour= datetime))
+
+plots = gps %>%
+  group_by(id) %>%
+  do(plots = p %+% . + facet_wrap(~deploy_id))
+plots$plots[[6]]
+
+pdf("output/figures/individbunny_GPSbydate_LGH.pdf",
+    width = 10, height = 7)
+plots$plots
+dev.off()
