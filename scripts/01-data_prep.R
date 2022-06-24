b4ba1303734273e13eb96eefba07decc18456668
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-24 15:17:53 -0600

    restructure data prep now that we aren't dealing with season

--- scripts/01-data_prep.R
+++ scripts/01-data_prep.R
@@ -20,68 +20,55 @@ gps <- rbindlist(ls.files, fill = TRUE, use.names = TRUE)
 
 # Reconstruct data -----------------------------------------------------
 
+#variables that we are splitting calculations by
+splitburst <- c("id", "winter", "burst")
+splityear <- c("id", "winter")
+
 #overwrite id column with animal
 gps[, id := animal]
 
-#categorize gps fixes into winters
-gps[idate > "2015-10-31" & idate < "2016-04-01", winter := "2015-2016"]
-gps[idate > "2016-10-31" & idate < "2017-04-01", winter := "2016-2017"]
-gps[idate > "2017-10-31" & idate < "2018-04-01", winter := "2017-2018"]
-gps[idate > "2018-10-31" & idate < "2019-04-01", winter := "2018-2019"]
-gps[idate > "2019-10-31" & idate < "2020-04-01", winter := "2019-2020"]
-gps[idate > "2020-10-31" & idate < "2021-04-01", winter := "2020-2021"]
-gps[idate > "2021-10-31" & idate < "2022-04-01", winter := "2021-2022"]
+#grab only fixes during winter
+gps <- gps[mnth == 11 | mnth == 12 | mnth == 1 | mnth == 2 | mnth == 3]
 
-#create early winter and late winter categories
-#fixes in nov and dec are "early"
-#fixes in feb and march are "late"
-gps[mnth == 11| mnth == 12, season := "early"]
-gps[mnth == 2| mnth == 3, season := "late"]
-
-#remove anything that doesn't fall into early or late winter
-gps <- gps[!is.na(season)]
-
-#variables that we are splitting calculations by
-splitburst <- c("id", "winter", "burst")
-splitseason <- c("id", "winter", "season")
-
-#calculate the difference in days from first day of a burst
-gps[, diffday := idate - min(idate), by = splitburst]
-
-#calculate the length of a season
-gps[, seasonlength := max(idate) - min(idate), by = splitburst]
-
-#calculate the length of each burst
-gps[, burstlength := max(idate) - min(idate), by = splitburst]
-
-#calculate the difference between days of fix, in order of datetime, by season
-setorder(gps, datetime)
-gps[, shiftdate := shift(idate), by = splitseason] #take date before , for each fix
-gps[, lagdate := idate - shiftdate] #calculate difference between previous date and current date
-
-#take max lag between fixes for each bunny-season
-gps[, maxlagdate := max(lagdate, na.rm = TRUE), by = splitseason]
-
-
-# Save compiled gps data --------------------------------------------------
-saveRDS(gps, "Data/all_gps.rds")
-
-
-#Liam Step length TA analysis
-
-#use only sample periods that are 10 days or more
-gps <- gps[burstlength >= 10]
-
+#remove any fixes not allocated to a burst
 gps <- gps[!is.na(burst)]
 
 
-#calculate the difference between times of fix, in order of datetime, by season
+#categorize fixes into winters
+gps[mnth > 9, winter := paste0(yr, "-", yr+1)]
+gps[mnth < 4, winter := paste0(yr-1, "-", yr)]
+
+#calculate the difference in days from first day of a burst within a winter
+gps[, diffday := idate - min(idate), by = splityear]
+
+#calculate the length of a full collar period per winter
+gps[, winterlength := max(idate) - min(idate), by = splityear]
+
+#calculate the length of each burst of collar data
+gps[, burstlength := max(idate) - min(idate), by = splitburst]
+
+
+
+# lag dates ---------------------------------------------------------------
+
+
+#calculate the difference between days of fix, in order of datetime, by burst
+#bursts with gaps greater than a day or two must be bursts where many fixes got removed in cleaning
 setorder(gps, datetime)
-gps[, nextfix := shift(datetime, n=1, type="lead"), by = .(id, winter, season, burst)] #take time before , for each fix
+gps[, shiftdate := shift(idate), by = splitburst] #take date before , for each fix
+gps[, lagdate := idate - shiftdate] #calculate difference between previous date and current date
+#take max lag between fixes for each bunny-winter
+gps[, maxlagdate := max(lagdate, na.rm = TRUE), by = splitburst]
+
+
+
+
+# fix rates, step length and speed ---------------------------------------------------------------
+
+gps[, nextfix := shift(datetime, n=1, type="lead"), by = splitburst] #take time before , for each fix
 gps[, fixrate := as.numeric(round(difftime(nextfix, datetime, units= 'mins')))] #calculate difference between previous time and current time
 hist(gps$fixrate)
 #a lot of zero fix rates - they should have been removed in prep_locs?
-
 
 #put next coordinates in new column
 setorder(gps, datetime)
@@ -91,15 +78,26 @@ gps[, next_y_proj := shift(y_proj, n=1, type="lead"), by = .(id, winter, season,
 #calculate step length
 gps[, S.L. := sqrt((next_x_proj - x_proj)^2 + (next_y_proj - y_proj)^2)] 
 
+#create speed column
+gps[, speed := S.L./fixrate, by= .(id, winter, season, burst)]
+
+
+
+# Save compiled gps data --------------------------------------------------
+saveRDS(gps, "Data/all_gps.rds")
+
+
+
+
+
+
+
 #grab important columns
 gps<-gps[, .(id, winter, season, burst, datetime, nextfix, fixrate, x_proj, next_x_proj, y_proj, next_y_proj, S.L.)]
 
 #remove SLs greater than 5000 meters
 gps<- gps[S.L. <= 5000]
 
-
-#create speed column
-gps[, speed := S.L./fixrate, by= .(id, winter, season, burst)]
 
 
 stats <- gps[, .(median(fixrate), median(S.L.), median(speed)), by = .(id, winter, season, burst)]
