a40c47a667234b943b2d852eeb9ea2a02fac4c97
Liam-Horne <lhorne@ualberta.ca>
2022-06-23 18:59:00 -0600

    added a bunch of scratch-work at bottom

--- scripts/03-calc_HRarea.R
+++ scripts/03-calc_HRarea.R
@@ -48,12 +48,17 @@ areas <- areas[HRninety < 90]
 saveRDS(areas, "output/results/hrareas.rds")
 
 
+
+
+
+
+
+
 #looking into HRs > 15 ha - Liam
 stancheck<-areas[HRninety >= 15]
 unique(stancheck$id)
 
 b23698<-gps[id == "23698" ]
-b23698<-b23698[x_proj > 300000] # remove one big outlier
 b25618<-gps[id == "25618"] 
 b26316<-gps[ id == "26316"]
 b26342<-gps[id == "26342"]
@@ -66,15 +71,23 @@ ggplot(b26383, aes(x=x_proj, y=y_proj)) +
 
 ggplot(b23698, aes(x=x_proj, y=y_proj)) +
      geom_point(aes(size=0.1, colour= datetime))#still other outliers - hopefully would get caught by out-and-back cleaning later
-ggplot(b25618, aes(x=x_proj, y=y_proj)) +
-      geom_point(aes(size=0.1, colour= datetime)) 
-ggplot(b26316, aes(x=x_proj, y=y_proj)) +
-      geom_point(aes(size=0.1, colour= datetime)) 
-ggplot(b26342, aes(x=x_proj, y=y_proj)) +
-      geom_point(aes(size=0.1, colour= datetime)) 
+
+hr19.20<-ggplot(b25618, aes(x=x_proj, y=y_proj)) +
+      geom_point(aes(size=0.1, colour= datetime)) + ggtitle("2019/20")  #2019/20
+hr20.21<-ggplot(b26316, aes(x=x_proj, y=y_proj)) +
+      geom_point(aes(size=0.1, colour= datetime)) + ggtitle("2020/21") #2020/21
+hr21.22<-ggplot(b26342, aes(x=x_proj, y=y_proj)) +
+      geom_point(aes(size=0.1, colour= datetime)) + ggtitle("2021/22")#2021/22
+
+ggsave("output/figures/hr19.20_90MCP.jpeg", hr19.20, width = 8, height = 6, units = "in")
+ggsave("output/figures/hr20.21_90MCP.jpeg", hr20.21, width = 8, height = 6, units = "in")
+ggsave("output/figures/hr21.22_90MCP.jpeg", hr21.22, width = 8, height = 6, units = "in")
 
 #looking into 2020-21 HRs- Liam
 w20.21<-gps[winter == "2020-2021"]
+unique(w20.21$id) #26316 26343 26344 26370 25226
+
+
 
 ggplot(w20.21, aes(x=x_proj, y=y_proj)) +
   geom_point(aes(size=0.1, colour= datetime, shape= as.factor(id))) 
@@ -86,12 +99,53 @@ gps<-as.data.frame(gps)
 p<-ggplot(gps, aes(x=x_proj, y=y_proj)) +
   geom_point(aes(size=0.1, colour= datetime))
 
+
 plots = gps %>%
   group_by(id) %>%
-  do(plots = p %+% . + facet_wrap(~deploy_id))
+  do(plots = p %+% . + facet_wrap(~deploy_id))#check misalignments between deploy_id date and actual date
 plots$plots[[6]]
 
-pdf("output/figures/individbunny_GPSbydate_LGH.pdf",
+pdf("output/figures/individbunny_GPSbydate_LGH-JUn23-2022.pdf",
     width = 10, height = 7)
 plots$plots
 dev.off()
+
+#check if can make mcp's
+# Only include three columns (id, x, and y coordinates) for making MCP's
+gps.sp <- gps[, c("id", "x_proj", "y_proj")] 
+coordinates(gps.sp) <- c("x_proj", "y_proj")
+proj4string(gps.sp) <- CRS(utm7N)
+
+gpsmcp <- mcp(gps.sp, percent = 90)
+gpsmcp
+
+plot(gps.sp, col = as.factor(gps.sp@data$id), pch = 16)
+plot(gpsmcp, col = alpha(1:5, 0.5), add = TRUE)
+
+#look at HR size changes based on MCP %
+hr<-mcp.area(gps.sp, percent = seq(50, 100, by = 5), plotit = FALSE)
+hr$perc<- seq(50, 100, by = 5)
+hr<-as.data.table(hr)
+hr.melt<-melt(hr, id.vars = c("perc"))
+
+ggplot(hr.melt)+
+  geom_line(aes(x = perc, y = value, group = variable))+
+  labs(y = "HR Size", x = "MCP Percentage") + ylim(0, 25)
+  theme_densities
+
+
+df <- fortify(gpsmcp)
+df$perc <- "90"
+
+
+g <- ggplot(df, aes(x = long, y = lat)) +
+  geom_polygon(alpha = 0, colour=c("red")) 
+
+plots = df %>%
+  group_by(id) %>%
+  do(plots = g %+% . + facet_wrap(~id))#check misalignments between deploy_id date and actual date
+plots$plots[[5]]
+
+
+g
+g + facet_wrap(~id)
