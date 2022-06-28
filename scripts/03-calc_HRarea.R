e6fb9dcc69ed5de1321b3daef384f0cecfab8423
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-06-27 20:56:02 -0600

    calculate home ranges by week

--- scripts/03-calc_HRarea.R
+++ scripts/03-calc_HRarea.R
@@ -1,4 +1,3 @@
-# Prep work ---------------------------------------------------------------
 
 #source the R folder to load any packages and functions
 lapply(dir('R', '*.R', full.names = TRUE), source)
@@ -6,39 +5,41 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 #read in prepped GPS data
 gps <- readRDS("Data/all_gps.rds")
 
+#remove any data that is in a week that didn't hit 7 days
+gps <- gps[weeklength > 6]
+
+#items we will measure home ranges by
+weeksplit <- c("id", "winter", "burst", "week", "weekdate")
 
 
-#remove outlier fixes based on distribution of easting and northings
-#eastings
-gps <- gps[x_proj > 300000 & x_proj < 342000] #removed 10
-#northings
-gps <- gps[y_proj > 6750000 & y_proj < 6770000] #removed 2 more
-#check why one mean y_lat became 0 when just y_lat was non-zero???
 
+# calculate weekly MCP areas ----------------------------------------------
 
 #MCP size at 90% and 50%, keep id, winter, season, and grid
 #save as RDS
 
 #MCP at 90%
-area90 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 90), by = .(id, winter, season, burst)]
+area90 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 90), by = weeksplit]
 setnames(area90, "a", "HRninety") #change column name
 
 #MCP at 75%
-area75 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 75), by = .(id, winter, season, burst)]
+area75 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 75), by = weeksplit]
 setnames(area75, "a", "HR75") #change column name
 
 #MCP at 50%
-area50 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 50), by = .(id, winter, season, burst)]
+area50 <- gps[, mcp_area(.SD, x = "x_proj", y = "y_proj", utmzone = utm7N, vol = 50), by = weeksplit]
 setnames(area50, "a", "HRfifty") #change column name
 
+
+
+
+# merge and save output ---------------------------------------------------
+
+
 #merge areas of 90% , 75% volume together
-areas.temp <- merge(area90, area75, by = c("id", "winter", "season", "burst"))
+areas.temp <- merge(area90, area75, by = weeksplit)
 #merge the 50% volume as well
-areas<- merge(areas.temp, area50, by = c("id", "winter", "season", "burst"))
-
-
-##### look into outlier
-areas <- areas[HRninety < 90]
+areas<- merge(areas.temp, area50, by = weeksplit)
 
 
 #save HR areas as an RDS file in the output folder
