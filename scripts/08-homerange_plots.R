5c5b0d9cd5d236d5835877f5ab51aa7220e98570
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-07-04 15:04:17 -0600

    new MCP plots

--- scripts/08-homerange_plots.R
+++ scripts/08-homerange_plots.R
@@ -7,54 +7,59 @@ DT[, Food := as.factor(Food)]
 
 DT <- DT[!HRninety > 20]
 
-(byyear <- 
+
+
+# 90% MCP  -------------------------------------------------------
+
+(byyear90 <- 
   ggplot(DT)+
   geom_boxplot(aes(x = winter, y = HRninety), outlier.shape = NA)+
-  geom_jitter(aes(x = winter, y = HRninety, color = winterday), alpha = .7, width = .3)+
+  geom_jitter(aes(x = winter, y = HRninety), alpha = .7, width = .3)+
   labs(y = "90% MCP area (ha)", x = "Winter")+
   theme_boxplots)
 
-
-(byhdensity <- 
+(byhdensity90 <- 
   ggplot(DT)+
   geom_point(aes(x = haredensity/10000, y = HRninety))+
   labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
   theme_densities)
 
-(byppratio <- 
+(byppratio90 <- 
   ggplot(DT)+
-  geom_point(aes(x = lynxdensity, y = HRninety))+
+  geom_point(aes(x = ppratio, y = HRninety))+
   labs(y = "90% MCP area (ha)", x = "Lynx:Hare Ratio")+
   theme_densities)
 
-(hrsize <- ggarrange(byyear, byhdensity, byppratio, ncol = 1, nrow = 3))
+(hrsize90 <- ggarrange(byyear90, byhdensity90, byppratio90, ncol = 1, nrow = 3))
 
 
-ggsave("output/figures/hrsize_90MCP_withseason.jpeg", hrsize, width = 6, height = 8, units = "in")
 
 
-#75% MCP plots
-(byyear <- 
+# 50% MCP -----------------------------------------------------------------
+
+
+(byyear50 <- 
     ggplot(DT)+
-    geom_boxplot(aes(x = winter, y = HR75), outlier.shape = NA)+
-    geom_jitter(aes(x = winter, y = HR75), alpha = .5, width = .3)+
-    labs(y = "75% MCP area (ha)", x = "Winter")+
+    geom_boxplot(aes(x = winter, y = HRfifty), outlier.shape = NA)+
+    geom_jitter(aes(x = winter, y = HRfifty), alpha = .7, width = .3)+
+    labs(y = "50% MCP area (ha)", x = "Winter")+
     theme_boxplots)
 
-
-(byhdensity <- 
+(byhdensity50 <- 
     ggplot(DT)+
-    geom_point(aes(x = haredensity/10000, y = HR75))+
-    labs(y = "75% MCP area (ha)", x = "Hare Density (hares per ha)")+
+    geom_point(aes(x = haredensity/10000, y = HRfifty))+
+    labs(y = "50% MCP area (ha)", x = "Hare Density (hares per ha)")+
     theme_densities)
 
-(byppratio <- 
+(byppratio50 <- 
     ggplot(DT)+
-    geom_point(aes(x = lynxdensity, y = HR75))+
-    labs(y = "75% MCP area (ha)", x = "Lynx:Hare Ratio")+
+    geom_point(aes(x = ppratio, y = HRfifty))+
+    labs(y = "50% MCP area (ha)", x = "Lynx:Hare Ratio")+
     theme_densities)
 
-(hrsize <- ggarrange(byyear, byhdensity, byppratio, ncol = 1, nrow = 3))
+(hrsize50 <- ggarrange(byyear50, byhdensity50, byppratio50, ncol = 1, nrow = 3))
 
 
-ggsave("output/figures/hrsize_75MCP_withseason.jpeg", hrsize, width = 6, height = 8, units = "in")
+
+ggsave("output/figures/hrsize_90MCP.jpeg", hrsize90, width = 6, height = 8, units = "in")
+ggsave("output/figures/hrsize_50MCP.jpeg", hrsize50, width = 6, height = 8, units = "in")
