a23f13b748c05be86e09f55ab09b445ff90c1001
Juliana Balluffi-Fry <jballuffi@gmail.com>
2022-07-05 08:45:42 -0600

    create model output summary and rename table

--- scripts/09-stats.R
+++ scripts/09-stats.R
@@ -31,19 +31,39 @@ resource <- lm(HRninety ~ mass + winterday + Food, dat)
 pred_res <- lm(HRninety ~ ppratio + mass + winterday + Food, dat )
 comp_res <- lm(HRninety ~ haredensity + mass + Food, dat)
 
-
+#list models and provide names
 mods <- list(predation, competition, resource, pred_res, comp_res)
-names <- c("predation", "competition", "resource", "pred_res", "comp_res")
+names <- c("predation", "competition", "resource", "predator and resource", "competition and resource")
+
+#create AIC table on list of models
 AIC<-as.data.table(aictab(REML=F, cand.set = mods, modnames = names, sort = TRUE))
 AIC[,ModelLik:=NULL]
 AIC[,Cum.Wt:=NULL]
-#round whole table to 3 dec places
-AIC<-AIC %>% mutate_if(is.numeric, round, digits=3)
+AIC<-AIC %>% mutate_if(is.numeric, round, digits=3) #round whole table to 3 digits
 
+#which models are less than 2 delta AIC? 
 topmods <- AIC[Delta_AICc < 2, return((Modnames))]
 
 
-#apply to same list of models as in AIC
-OutAll<-lapply(mods, lm_out)
-OutAll<-rbindlist(OutAll, fill = TRUE)
-OutAll$Model<-names
+#apply the lm_out function to the top to same list of models as in AIC
+outall <- lapply(mods, lm_out)
+outall <- rbindlist(outall, fill = TRUE)
+outall$Model <- names
+outall[, `(Intercept)` := NULL]
+
+
+
+#function to swap out specific words in column names for new ones
+nameswap <- function(old, new, Data) {
+  older<-colnames(Data)[grep(old, colnames(Data))]
+  newer <- gsub(old, new, older)
+  setnames(Data, older, newer)
+}
+
+#change names of columns in output table
+nameswap("Food1", "Food add", outall)
+nameswap("winterday", "Day", outall)
+nameswap("mass", "Body mass", outall)
+nameswap("haredensity", "Hare density", outall)
+nameswap("ppratio", "Lynx:hare ratio", outall)
+nameswap("rsq", "R2", outall)
