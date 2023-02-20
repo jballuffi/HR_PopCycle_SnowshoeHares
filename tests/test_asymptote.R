4f1d988481233dc71b8c6f44b5bf5aef129e121d
Juliana Balluffi-Fry <jballuffi@gmail.com>
2023-02-19 22:00:05 -0700

    ggplot sample effort

--- tests/test_asymptote.R
+++ tests/test_asymptote.R
@@ -17,12 +17,22 @@ gps <- readRDS("Data/all_gps.rds")
 
 onebun <- gps[id %in% sample(unique(gps$id), 1, replace = FALSE)]
 
-morebun <- gps[id %in% sample(unique(gps$id), 5, replace = FALSE)] 
+morebun <- gps[id %in% sample(unique(gps$id), 10, replace = FALSE)] 
 
 
-# Tests -------------------------------------------------------------------
+# Tests ----- --------------------------------------------------------------
 
 
 out1 <- area_asym(onebun)
 
-outmore <- morebun[, area_asym(DT = .SD), by = c("id", "winter", "season")]
+outmore <- morebun[, area_asym(DT = .SD), by = c("id", "winter")]
+
+outmore$id <- as.factor(outmore$id)
+
+
+ggplot(outmore)+
+  geom_path(aes(x = daycount, y = area, group = id, color = id))
+
+
+full <- gps[, area_asym(DT = .SD), by = c("id", "winter")]
+
