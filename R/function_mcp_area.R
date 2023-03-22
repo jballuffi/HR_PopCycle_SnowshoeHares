b149abd685e159be3c54a5d6ec161a8a1b520dd9
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-03-22 16:55:32 -0600

    comment change

--- R/function_mcp_area.R
+++ R/function_mcp_area.R
@@ -1,6 +1,6 @@
 #function that calculates mcp area from a data.table or data.frame
 #Arguments: x = x coord; y = y coord; col1-col3 = additional data to include
-# utmzone = the utm zone; df = the data.table
+# utmzone = the utm zone; gpsdata = the data.table
 
 mcp_area <- function(gpsdata, x, y, utmzone, vol) {
   #first convert to a spatial points data frame
