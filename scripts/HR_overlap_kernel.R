9e359bfae5b3fd8ddc22958d42200ce1ec295e31
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-03-23 17:00:31 -0600

    create function out of code that actually works

--- scripts/HR_overlap_kernel.R
+++ scripts/HR_overlap_kernel.R
@@ -8,30 +8,26 @@ gps <- readRDS("Data/all_gps.rds")
 #remove any data that is in a week that didn't hit 7 days
 gps <- gps[weeklength > 6]
 
-#items we will measure home ranges by
-weeksplit <- c("id", "winter", "burst", "week", "weekdate")
+
+subset <- gps[winter == "2015-2016"]
 
 
-subset <- gps[winter == "2015-2016" & burst == 1 & week == "(-1,7]"]
+HR_overlap <- function(DT){
+  data.xy <- DT[, .(x_proj, y_proj)]
+  xysp <- SpatialPoints(data.xy)
+  proj4string(xysp) <- CRS(utm7N)
+  sppt <- data.frame(xysp)
+  idsp <- subset[, .(id)]
+  coordinates(idsp)<-sppt
+  ud <- kernelUD(idsp[,1])
+  kernelUD(idsp[,1], h = "href", grid = 200, same4all = TRUE, hlim = c(0.1, 1.5),
+           kern = c("bivnorm"), extent = 0.5)
+  overlap <- kerneloverlap(idsp[,1], grid=200, method="HR", percent=90, conditional=TRUE)
+  return(overlap)
+}
 
 
-# spdf <- Spat(utm7N)
-#spatialPointsDataFrame(subset[, .SD, .SDcols = c("x_proj", "y_proj")],
-#                                data = subset,
-#                                proj4string = CRS(utm7N))
+out <- subset[, HR_overlap(DT = .SD)]
+#this works but it doesnt work when you try to run the function by anything. whats up with that?
 
-
-
-data.xy <- subset[, .(x_proj, y_proj)]
-xysp <- SpatialPoints(data.xy)
-proj4string(xysp) <- CRS(utm7N)
-sppt <- data.frame(xysp)
-idsp <- subset[, .(id)]
-coordinates(idsp)<-sppt
-ud <- kernelUD(idsp[,1])
-kernelUD(idsp[,1], h = "href", grid = 200, same4all = TRUE, hlim = c(0.1, 1.5),
-         kern = c("bivnorm"), extent = 0.5)
-image(ud)
-
-overlap <- kerneloverlap(idsp[,1], grid=200, method="HR", percent=95, conditional=TRUE)
 
