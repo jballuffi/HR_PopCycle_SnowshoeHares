552d91a287703531ff8a8c9c8b1cc988b0b9c5f2
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-03-27 16:18:39 -0600

    swap winter day for snow depth

--- scripts/10-stats.R
+++ scripts/10-stats.R
@@ -5,41 +5,43 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 #read in data
 dat <- readRDS("output/results/compileddata.rds")
-dat <- dat[!HRninety > 20]
+dat <- dat[!M90 > 20]
+#reorder phase cycles
+dat[, phase := factor(phase, levels = c("increase", "peak", "decrease", "low"))]
 
 
 
 # correlation test --------------------------------------------------------
 
 #subset data to only variables that we need to test co linearity on (numeric only)
-forcor <- dat[, .(haredensity, lynxdensity, ppratio, mass, winterday)]
+forcor <- dat[, .(haredensity, lynxdensity, ppratio, mass, SD)]
 
 #run correlation, look at matrix style output
 round(cor(forcor, use = "complete.obs"), digits = 2)
-
-#winter day correlates with hare density 
-#hare density correlates with lynx density
 
 
 
 # AIC for analysis across winters -----------------------------------------
 
 #list models
-predation <- lm(HRninety ~ ppratio, dat)
-competition <- lm(HRninety ~ haredensity, dat)
-resource <- lm(HRninety ~ mass + winterday + Food, dat)
-pred_res <- lm(HRninety ~ ppratio + mass + winterday + Food, dat )
-comp_res <- lm(HRninety ~ haredensity + mass + Food, dat)
+cycle <- lm(M90 ~ phase, dat)
+predation <- lm(M90 ~ ppratio, dat)
+competition <- lm(M90 ~ haredensity, dat)
+resource <- lm(M90 ~ mass + SD + Food, dat)
+pred_res <- lm(M90 ~ ppratio + mass + SD + Food, dat)
+comp_res <- lm(M90 ~ haredensity + mass + SD + Food, dat)
+cycle_res <- lm(M90 ~ phase + mass + SD + Food, dat)
 
 #list models and provide names
-mods <- list(predation, competition, resource, pred_res, comp_res)
-names <- c("predation", "competition", "resource", "predator and resource", "competition and resource")
+mods <- list(cycle, predation, competition, resource, pred_res, comp_res, cycle_res)
+names <- c("cycle", "predation", "competition", "resource", 
+           "predator and resource", "competition and resource", "cycle and resource")
 
 #create AIC table on list of models
-AIC<-as.data.table(aictab(REML=F, cand.set = mods, modnames = names, sort = TRUE))
-AIC[,ModelLik:=NULL]
-AIC[,Cum.Wt:=NULL]
-AIC<-AIC %>% mutate_if(is.numeric, round, digits=3) #round whole table to 3 digits
+AIC<-as.data.table(aictab(REML = F, cand.set = mods, modnames = names, sort = TRUE))
+AIC[, ModelLik := NULL]
+AIC[, Cum.Wt := NULL]
+AIC <- AIC %>% mutate_if(is.numeric, round, digits=3) #round whole table to 3 digits
 
 #which models are less than 2 delta AIC? 
 topmods <- AIC[Delta_AICc < 2, return((Modnames))]
