651b3808655bae12bee0116be9b2a9c1877224ea
Juliana Balluffi-Fry <jballuffi@gmail.com>
2023-03-28 11:12:03 -0600

    create figure showing hr size by cycle and resources

--- scripts/09-homerange_plots.R
+++ scripts/09-homerange_plots.R
@@ -22,19 +22,9 @@ DT[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
 
 
 
-# Food add and sex --------------------------------------------------------
+# Home range in response to density parameters  -------------------------------------------------------
 
-(foodadd <-
-  ggplot(DT[!is.na(sex)])+
-  geom_boxplot(aes(x = sex, y = M90, color = Food))+
-  labs(y = "90% MCP area (ha)", x = "Sex")+
-  theme_boxplots)
-
-
-
-# 90% MCP  -------------------------------------------------------
-
-(byyear90 <- 
+(byyear <- 
   ggplot(DT)+
   geom_boxplot(aes(x = winter, y = M90))+
   #geom_jitter(aes(x = winter, y = HRninety), alpha = .7, width = .3)+
@@ -42,7 +32,7 @@ DT[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
   theme_boxplots+
    theme(axis.text.x.bottom = element_text(size = 8)))
 
-(byhdensity90 <- 
+(byhdensity <- 
   ggplot(DT)+
   geom_point(aes(x = haredensity/10000, y = M90))+
   labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
@@ -60,14 +50,37 @@ DT[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
   labs(y = "90% MCP area (ha)", x = "Cycle Phase")+
   theme_boxplots)
 
-(hrsize90 <- ggarrange(byyear90, byhdensity90, byldensity, byphase,
+(hrcycle <- ggarrange(byyear, byhdensity, byldensity, byphase,
                        ncol = 2, nrow = 2))
 
 
 
+# Home range in response to resource parameters --------------------------------------------------------
+
+(byfood <-
+   ggplot(DT[!is.na(sex)])+
+   geom_boxplot(aes(x = sex, y = M90, color = Food))+
+   labs(y = "90% MCP area (ha)", x = "Sex")+
+   theme_boxplots)
+
+(bysnow <- 
+  ggplot(DT)+
+  geom_point(aes(x = SD, y = M90, color = phase))+
+  labs(y = "90% MCP area (ha)", x = "Snow depth (cm)")+
+  theme_densities)
+
+(bymass <- 
+  ggplot(DT)+
+  geom_point(aes(x = mass, y = M90, color = phase))+
+  labs(y = "90% MCP area (ha)", x = "Body mass (g)")+
+  theme_densities)
+
+(hrresource <- ggarrange(byfood, bysnow, bymass, ncol = 1, nrow = 3))
+
+
 # save figures ------------------------------------------------------------
 
-ggsave("output/figures/hrsize_90MCP.jpeg", hrsize90, width = 10, height = 6, units = "in")
+ggsave("output/figures/HRbycycle.jpeg", hrcycle, width = 10, height = 6, units = "in")
 
-ggsave("output/figures/foodadd_90MCP.jpeg", foodadd, width = 6, height = 6, units = "in")
+ggsave("output/figures/HRbyresource.jpeg", hrresource, width = 5, height = 8, units = "in")
 
