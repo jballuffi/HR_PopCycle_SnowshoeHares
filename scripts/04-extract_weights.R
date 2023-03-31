b541c27e433d2463b4c6c67cc34b96af3c669f62
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-03-31 11:35:59 -0600

    change weight data to include all measures, not just winter means

--- scripts/04-extract_weights.R
+++ scripts/04-extract_weights.R
@@ -8,11 +8,8 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 trapping <- fread("data/Trapping_data_all_records.csv")
 #for sex column, 1 = male, 2 = female, 0 = no data
 
-
 #read in HR areas
 areas <- readRDS("output/results/hrareas.rds")
-
-
 
 
 # clean trapping data -----------------------------------------------------
@@ -47,11 +44,7 @@ t2[mnth > 9, winter := paste0(year(date), "-", year(date)+1)] #grab october beca
 t2[mnth < 4, winter := paste0(year(date)-1, "-", year(date))]
 
 #remove anything that doesn't fall within winter
-t2 <- t2[!is.na(winter)]
+weights <- t2[!is.na(winter)]
 
-
-#calc body mass by id, winter and season
-weights <- t2[, .(mean(Weight), getmode(Sex)), by = .(id, winter)]
-setnames(weights, c("V1", "V2"), c("mass", "sex"))
 
 saveRDS(weights, "output/results/bodymass.rds")
