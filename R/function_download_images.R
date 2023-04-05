c37669016c8149d9073f962e13427df98f789c39
Liam-Horne <lhorne@ualberta.ca>
2023-04-05 15:06:12 -0600

    removed argumetns and changed col names outside of function
    This is how other functions do it. It's too many arguments to have, and I can't do a 'setnames' inside the forloop

--- R/function_download_images.R
+++ R/function_download_images.R
@@ -12,10 +12,9 @@
 # Author: Liam Horne
 # Last Updated: April 4, 2023
 
-download.images<- function(dat, file.path, loc.col, url.col) {
+download.images<- function(dat, file.path) {
   
   #get list of locs
-  setnames(dat, loc.col, "location")
   loc <- dat[, unique(location)]
   
   for(l in loc) {
@@ -28,30 +27,29 @@ download.images<- function(dat, file.path, loc.col, url.col) {
     if (already.directory) { #then just add folders to the directory already named as the loc
       
       #get image urls from this location
-      setnames(specloc, url.col, "image_url")
       url<-specloc[, image_url]
       
       for(u in url) {
         
-        #get the date from the row with the'u-th' URL
-        d<-specloc[image_url == u, date]  #just date, not .(date) because just want the value output
+        #get the image id from the row with the 'u-th' URL
+        iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
         #download image from url
-        download.file(u, destfile = paste0(file.path, l, "/image_", d, ".jpg"), mode = 'wb')
+        download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
       }
-    } else { #if the directory doesnt exist yet
+    } else { #if the directory doesn't exist yet
       
       #create folder in directory for this location
       dir.create(paste0(file.path, l))
       
       #get image urls from this location
-      setnames(specloc, url.col, "image_url")
       url<-specloc[, image_url]
       
       for(u in url) {
-        #get the date from the row with the'u-th' URL
-        d<-specloc[image_url == u, date]  #just date, not .(date) because just want the value output
+        
+        #get the image id from the row with the 'u-th' URL
+        iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
         #download image from url
-        download.file(u, destfile = paste0(file.path, l, "/image_", d, ".jpg"), mode = 'wb')
+        download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
       }
     }
   }
@@ -61,14 +59,21 @@ download.images<- function(dat, file.path, loc.col, url.col) {
 
 
 #TEST
-test<-sd3r[c(236:239)]
-test[1, `image_url(admin only)` := "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-test[2, `image_url(admin only)` := "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-test[3, `image_url(admin only)` := "https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-test[4, `image_url(admin only)` := "https://images.pexels.com/photos/2088210/pexels-photo-2088210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-test[, location := c("A1", "A2", "A2", "A3")]
-test1<-test[, .(location, date`image_url(admin only)`)]
-names(test1)
-download.images(dat=test1, file.path = "Data/liam_test_data/test_func/", loc.col = "location",
-                url.col = "image_url(admin only)")
+# test<-sd3r[c(236:239)]
+# test[1, `image_url(admin only)` := "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+# test[2, `image_url(admin only)` := "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+# test[3, `image_url(admin only)` := "https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+# test[4, `image_url(admin only)` := "https://images.pexels.com/photos/2088210/pexels-photo-2088210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+# test[, location := c("A1", "A2", "A2", "A3")]
+# setnames(test, "location", "camera_site")
+# 
+# #create the export I will put on GitHub as example
+# test1<-test[, .(camera_site, date,`image_url(admin only)`)]
+# names(test1)
+# setnames(test1, "camera_site", "location") #Change the name of the column that has your camera locations/names
+# setnames(test1, "image_url(admin only)", "image_url") #change the name of the column that has your image URLs
+# setnames(test1, "date", "image_id") #change the name of the column that has your image id info (e.g., date), which will be put into the file name of each image
+# 
+# download.images(dat=test1, file.path = "Data/liam_test_data/test_func/")
+
 
