a65a544b184422f88006e79863daa4b605b50c63
Liam-Horne <lhorne@ualberta.ca>
2023-04-05 14:42:45 -0600

    trying to make more universal

--- R/function_download_images.R
+++ R/function_download_images.R
@@ -1,7 +1,7 @@
 #Function to download images from wildtrax
 # 'dat' argument is the 'image_report' excel sheet from the data download on Wildtrax
-# 'file.path' argument is the main folder where you want everything to be saved  (e.g., "Data/test_data/test_func/")
-  #separate folders will be automatically made for each camera location
+# 'file.path' argument is the primary folder within your R Project where you want everything to be saved (e.g., "Data/test_data/test_func/")
+      # Separate folders will be automatically made for each camera location
 
 
 #IMPORTANT NOTE: You need to have the package "data.table" for this function to work. Code for this below.
@@ -12,9 +12,10 @@
 # Author: Liam Horne
 # Last Updated: April 4, 2023
 
-download.images<- function(dat, file.path) {
+download.images<- function(dat, file.path, loc.col, url.col) {
   
   #get list of locs
+  setnames(dat, loc.col, "location")
   loc <- dat[, unique(location)]
   
   for(l in loc) {
@@ -26,12 +27,14 @@ download.images<- function(dat, file.path) {
     
     if (already.directory) { #then just add folders to the directory already named as the loc
       
-      #get image urls from this lcoation
-      url<-specloc[, `image_url(admin only)`]
+      #get image urls from this location
+      setnames(specloc, url.col, "image_url")
+      url<-specloc[, image_url]
       
       for(u in url) {
+        
         #get the date from the row with the'u-th' URL
-        d<-specloc[`image_url(admin only)` == u, date]  #just date, not .(date) because just want the value output
+        d<-specloc[image_url == u, date]  #just date, not .(date) because just want the value output
         #download image from url
         download.file(u, destfile = paste0(file.path, l, "/image_", d, ".jpg"), mode = 'wb')
       }
@@ -40,18 +43,32 @@ download.images<- function(dat, file.path) {
       #create folder in directory for this location
       dir.create(paste0(file.path, l))
       
-      #get image urls from this lcoation
-      url<-specloc[, `image_url(admin only)`]
+      #get image urls from this location
+      setnames(specloc, url.col, "image_url")
+      url<-specloc[, image_url]
       
       for(u in url) {
         #get the date from the row with the'u-th' URL
-        d<-specloc[`image_url(admin only)` == u, date]  #just date, not .(date) because just want the value output
+        d<-specloc[image_url == u, date]  #just date, not .(date) because just want the value output
         #download image from url
         download.file(u, destfile = paste0(file.path, l, "/image_", d, ".jpg"), mode = 'wb')
       }
-      
     }
-    
   }
-  
 }
+
+# download.images(dat=test, file.path = "Data/liam_test_data/test_func/", url.col= "image_url(admin only)")
+
+
+#TEST
+test<-sd3r[c(236:239)]
+test[1, `image_url(admin only)` := "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+test[2, `image_url(admin only)` := "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+test[3, `image_url(admin only)` := "https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+test[4, `image_url(admin only)` := "https://images.pexels.com/photos/2088210/pexels-photo-2088210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+test[, location := c("A1", "A2", "A2", "A3")]
+test1<-test[, .(location, date`image_url(admin only)`)]
+names(test1)
+download.images(dat=test1, file.path = "Data/liam_test_data/test_func/", loc.col = "location",
+                url.col = "image_url(admin only)")
+
