0dbadb93a6bf0bcd7a360b51c3d4dbb6f18fd036
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-04-11 15:27:53 -0600

    remove transect stuff

--- scripts/06-snow_depth.R
+++ scripts/06-snow_depth.R
@@ -19,10 +19,8 @@ snows <- rbindlist(ls.snowfiles, fill = TRUE, use.names = TRUE, idcol = "origin"
 snows[, origin := factor(origin, labels = basename(snowfiles))]
 
 
-#data from transect measures
-sd1 <- fread("data/Kluane_PredTrans_SnowDepth.csv")
-sd2 <- fread("data/snowdepthdataentry.csv")
-
+#data from camera traps
+snowcams <- fread("data/Snow_cameras.csv")
 
 
 # clean up grid data -----------------------------------------------------------
@@ -45,79 +43,16 @@ setorder(gsnow, snowgrid, Date)
 
 
 
-# clean up transect data --------------------------------------------------
-
-#rename cols
-names(sd2) <- c("where", "Date", "SD", "Comments")
-
-#make date a date
-sd2[, Date := dmy(Date)]
-
-#clear old data out
-sd2 <- sd2[Date >= "2015-11-01"]
-
-#remove comments
-sd2[, Comments := NULL]
-
-#define the last date in Alice's data
-enddate <- sd2[, max(Date)]
-
-#add  label for transect data (just the segment because that is a unique measure and
-#tells that it is from pred transect data)
-sd1[, where := Segment]
-
-#convert to date
-sd1[, Date := dmy(DateTrans)]
-
-#remove data from before November 1, 2015 (the cutoff date for HR analysis)
-sd1 <- sd1[Date >= "2015-11-01"]
-
-#remove NA snow depths 
-sd1[!is.na(SnowDepthStn), unique(year(Date))] #(ask Alice about no data 2017-19) 
-sd1 <- sd1[!is.na(SnowDepthStn)]
-
-#remove repeat snow measure per segment
-sd1 <- sd1[, unique(SnowDepthStn), by = .(Date, where)]
-setnames(sd1, "V1", "SD")
-
-#subset to only dates after SD2 ends
-sd1late <- sd1[Date > enddate]
-
-#merge two dts together and remove faulty values (36 where sdepth = -9999)
-tsnow <- rbind(sd2, sd1late)
-tsnow <- tsnow[!SD < 0]
-
-#avg across locations by date
-tsnow <- tsnow[, mean(SD), by = Date]
-setnames(tsnow, "V1", "SD")
-
-#create a grid column
-tsnow[, snowgrid := "Transect"]
-
-
-# Test correlation between transect data and grid data --------------------
-
-#take mean snow depth across all grids by date
-gridmean <- gsnow[, mean(SD), Date]
-setnames(gridmean, "V1", "gridSD")
-
-#merge with transect data by date
-datatest <- merge(gridmean, tsnow, by = "Date", all.y = TRUE)
-
-ggplot(datatest)+
-  geom_point(aes(x = SD, y = gridSD))
+# clean up camera trap data --------------------------------------------------
 
 
 
-# final merge of transect and grid data -----------------------------------
 
 
-snow <- rbind(gsnow, tsnow)
 
-#categorize fixes into winters
-snow[month(Date) > 10, winter := paste0(year(Date), "-", year(Date) + 1)]
-snow[month(Date) < 4, winter := paste0(year(Date) - 1, "-", year(Date))]
-snow <- snow[!is.na(winter)]
+
+
+# figure and save ---------------------------------------------------------
 
 #figure to show snow depth over winter by year and location
 ggplot(snow)+
