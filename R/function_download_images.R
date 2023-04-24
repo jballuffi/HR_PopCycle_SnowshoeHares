fc5be34c3bf7bf3c6b8617dfacd4af0a23f6e79d
Liam-Horne <lhorne@ualberta.ca>
2023-04-24 14:28:52 -0600

    updated download images function from my main git

--- R/function_download_images.R
+++ R/function_download_images.R
@@ -1,80 +1,131 @@
-#Function to download images from Wildtrax
-# 'dat' argument is the 'image_report' excel sheet from a data download on Wildtrax (need admin priveleges to access image URLs)
-# 'file.path' argument is the primary folder within your R Project where you want the images to be saved 
-    # (e.g., "Data/cameras/" - make sure it ends with a "/")
-    # Separate folders will be automatically made for each camera location
+#Function to download images from a spreadsheet containing URLs for multiple images
+
+## Author: Liam Horne
+# Last Updated: April 17, 2023
+
+# ARGUMENTS ---------------------------------------------------------------
+
+# "dat" argument is the "image_report" spreadsheet from a data download on Wildtrax (you need organization admin privileges to access image URLs)
+
+# "file.path" argument is the existing main folder within your R Project where you want the images to be saved 
+# (e.g. if working within a R project, "Data/cameras/" - make sure it ends with a "/")
+# Separate folders will be automatically made for each camera location
 
 
-#IMPORTANT NOTE: You need to have the package "data.table" for this function to work.
+# IMPORTANT NOTES -------------------------------------------------------------
+
+# This function depends on "data.table" and "httr" packages. These dependencies are installed by the "download.images" function,
+# but you will still have to install "data.table" if you want to run the code to change column names following the
+# "Example data for download images function" script.
+
 # install.packages("data.table")
 # library(data.table)
 
+#The "dat" data table MUST HAVE 3 columns named "location", "image_url", and "image_id" for the columns containing the
+#camera location names, image URLs, and image IDs, respectively
+#This can be done using the "setnames" function in data.table - see the "Example data for download images function" script
 
-# Author: Liam Horne
-# Last Updated: April 5, 2023
+# Any of these characters \ / : * ? " < > | in the "location" or "image_id" columns
+#will cause errors and are therefore automatically replaced with "_" by the function
+
+
+# FUNCTION ----------------------------------------------------------------
 
 download.images<- function(dat, file.path) {
   
-  #get list of locs
+  #install httr and data.table packages if not already
+  packages <- c("httr", "data.table")
+  
+  # Loop through the packages and check if they are installed
+  for (pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      # If the package is not installed, install it
+      install.packages(pkg)
+      # Load the package
+      library(pkg, character.only = TRUE)
+    }
+  }
+  
+  #create function to see if any URLs dont exist
+  url_exists <- function(url) {
+    res <- tryCatch({
+      response <- HEAD(url)
+      status <- response$status_code
+      return(status == 200) #returns FALSE if there is a specific error status code (e.g., 404 Not Found)
+    }, error = function(e) {
+      return(FALSE) #returns FALSE if there is a non-specific error
+    })
+    return(res)
+  }
+  
+  #create empty data.table for data with bad URLs
+  op <- data.table()
+  
+  #replace any characters in location or image_id that are not allowed in file/folder names
+  dat[, location := gsub("[\\/:*?\"<>|]", "_", location)]
+  dat[, image_id := gsub("[\\/:*?\"<>|]", "_", image_id)]
+  
+  #get list of camera locations names
   loc <- dat[, unique(location)]
   
   for(l in loc) {
     #get  data for specific location
     specloc <- dat[location == l]
     
-    #check if dir exists
+    #check if directory exists
     already.directory<-dir.exists(paste0(file.path, l))
     
-    if (already.directory) { #then just add folders to the directory already named as the loc
+    if (already.directory) { #If True, then just add folders to the directory already named as given location in the loop
       
-      #get image urls from this location
+      #get image urls from the given location in the loop
       url<-specloc[, image_url]
       
       for(u in url) {
         
-        #get the image id from the row with the 'u-th' URL
-        iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
-        #download image from url
-        download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
+        #Check if URL actually exists (i.e is downloadable)
+        url.is.real <- url_exists(u)
+        
+        if(url.is.real) { #If TRUE, then download as normal
+          
+          #get the image id from the row with the given URL
+          iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
+          #download image from url, and name it according to the camera location and image id
+          download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
+          
+        } else { #if the URL doesn't exist, then just record the data for end output
+          bad<-dat[image_url == u]
+          op <- rbind(bad, op)
+        }
       }
     } else { #if the directory doesn't exist yet
       
-      #create folder in directory for this location
+      #create folder in directory for the given location
       dir.create(paste0(file.path, l))
       
-      #get image urls from this location
+      #get image urls from given location
       url<-specloc[, image_url]
       
       for(u in url) {
         
-        #get the image id from the row with the 'u-th' URL
-        iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
-        #download image from url
-        download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
+        #Check if URL actually exists (i.e is downloadable)
+        url.is.real <- url_exists(u)
+        
+        if(url.is.real) { #If TRUE, then download as normal
+          
+          #get the image id from the row with the given URL
+          iid<-specloc[image_url == u, image_id]  #just image_id, not .(image_id) because just want the 'Values' output
+          #download image from url, and name it according to the camera location and image id
+          download.file(u, destfile = paste0(file.path, l, "/image_", iid, ".jpg"), mode = 'wb')
+          
+        } else { #if the URL doesn't exist, then just record the data for end output
+          bad<-dat[image_url == u]
+          op <- rbind(bad, op)
+        }
       }
     }
   }
+  return(op)
 }
 
-# download.images(dat=test, file.path = "Data/liam_test_data/test_func/", url.col= "image_url(admin only)")
-
-
-#TEST
-# test<-sd3r[c(236:239)]
-# test[1, `image_url(admin only)` := "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-# test[2, `image_url(admin only)` := "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-# test[3, `image_url(admin only)` := "https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-# test[4, `image_url(admin only)` := "https://images.pexels.com/photos/2088210/pexels-photo-2088210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
-# test[, location := c("A1", "A2", "A2", "A3")]
-# setnames(test, "location", "camera_site")
-# 
-# #create the export I will put on GitHub as example
-# test1<-test[, .(camera_site, date,`image_url(admin only)`)]
-# names(test1)
-# setnames(test1, "camera_site", "location") #Change the name of the column that has your camera locations/names
-# setnames(test1, "image_url(admin only)", "image_url") #change the name of the column that has your image URLs
-# setnames(test1, "date", "image_id") #change the name of the column that has your image id info (e.g., date), which will be put into the file name of each image
-# 
-# download.images(dat=test1, file.path = "Data/liam_test_data/test_func/")
 
 
