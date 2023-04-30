228484d89db2a687161733ce1dd19629cb327637
Liam-Horne <lhorne@ualberta.ca>
2023-04-29 19:06:26 -0600

    script to look at metadata diffs. Messy, but maybe 37 collars we dont have?

--- extra_scripts/comparing sho metadata.R
+++ extra_scripts/comparing sho metadata.R
@@ -9,17 +9,19 @@ sm1518[, source:="mike"]
 sy1518 <- fread("data/liam_test_data/Sho2015-18_Yassheet.csv")
 sy1518 <- sy1518[, 1:28]
 sy1518[, source := "yas"]
-sr1518 <- fread("data/liam_test_data/Sho2015-18_rolosheet.csv") #this looks like just AXY data
+# sr1518 <- fread("data/liam_test_data/Sho2015-18_rolosheet.csv") #this looks like just AXY data
 
 s1819 <- fread("data/liam_test_data/Sho2018-19.csv")
 s1819[, source := "sho"]
 
 
 #convert to date
-ours[, Date := lubridate::dmy(`Date GPS On`)]
+ours[, Date := lubridate::dmy(`Date Off Bunny`)]
 ours[, yr := lubridate::year(Date)]
-ours<-ours[yr <=2019]
+ours[, mnth := lubridate::month(Date)]
+ours<-ours[yr <=2019 ]
 ours[, source := "us"]
+
 
 
 #just trying to get sho's data because there were multiple sheets
@@ -28,7 +30,7 @@ all<-rbind(smy1518, s1819)
 
 mys<-all[, .(`Bunny ID`, GRID, GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`, source)]
 mys[, dup:=duplicated(`Bunny ID`), by=.(GRID, GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`)]
-mys <- my[!is.na(`Bunny ID`)]
+mys <- mys[!is.na(`Bunny ID`)]
 mys[, unique(dup)]
 
 #remove non deployed collars
@@ -36,13 +38,13 @@ mys[, unique(`Bunny ID`)]
 mys<-mys[`Bunny ID` != "Not Deployed"]
 
 #remove rows with no GPS
-mys<-mys[`Date GPS On` != "-"] #some say AUDIO as the GPS ID
+mys<-mys[`Date GPS On` != "-"] #some say AUDIO as the GPS ID but have a Date GPS On
 mys<-mys[`Date GPS On` != ""]
 mys[, dup2:=duplicated(`Bunny ID`), by=.(GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`)]
 mys[, unique(dup2)]
 mys[is.na(GPS), GPS := "NoID"]
-mys<-mys[, .(`Bunny ID`, GRID, GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`, source)]
-
+mys<-mys[, .(`Bunny ID`, GRID, GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`, source)] #remove dup cols
+setnames(mys, "Bunny ID", "id")
 
 mys[, unique(GRID)]
 ours[, unique(GRID)]
@@ -50,11 +52,25 @@ ours[, unique(GRID)]
 sub.ours<-ours[, .(`Bunny ID`, GRID, GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`, source)]
 sub.ours[is.na(GPS), GPS := "NoID"]
 
+#make data formats consistent
+sub.ours[, id := gsub('B', '2', `Bunny ID`)]
+sub.ours[, start.date := lubridate::dmy(`Date on Bunny`)]
+sub.ours[, end.date := lubridate::dmy(`Date Off Bunny`)]
+sub.ours[, date.gps.on := lubridate::dmy(`Date GPS On`)]
+sub.ours<-sub.ours[, .(id, GRID, GPS, start.date, end.date, date.gps.on, source)]
+
+
+mys[, start.date := lubridate::dmy(`Date on Bunny`)]
+mys[, end.date := lubridate::dmy(`Date Off Bunny`)]
+mys[, date.gps.on := lubridate::dmy(`Date GPS On`)]
+mys<-mys[, .(id, GRID, GPS, start.date, end.date, date.gps.on, source)]
+
+
 #test if our and theirs line up
 com<-rbind(sub.ours, mys)
 com[, dup:=duplicated(`Bunny ID`), by=.(GPS, `Date GPS On`, `Date on Bunny`, `Date Off Bunny`)]
 
-com2<-merge(sub.ours, mys, by=c("Bunny ID", "Date GPS On", "Date on Bunny", "Date Off Bunny"), all=T)
-com2[is.na(source.x) | is.na(source.y)]
+com2<-merge(sub.ours, mys, by=c("id", "start.date", "end.date", "date.gps.on", "GPS", "GRID"), all=T)
+op<-com2[is.na(source.x) | is.na(source.y)]
 com2[is.na(source.x)]
 com2[is.na(source.y)]
