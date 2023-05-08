4e7495fecacca540f3e74644fdd11d27326fdd3c
Liam-Horne <lhorne@ualberta.ca>
2023-05-08 10:50:21 -0600

    updated script and added plots of all HRs

--- extra_scripts/HR_daily move.R
+++ extra_scripts/HR_daily move.R
@@ -62,9 +62,12 @@ buns.unnest30[is.na(burst_)] #no NAs for burst_
 
 bsl<-copy(buns.unnest30)
 
-#merge by deploy_id to get all dates and week for that ID 
+# saveRDS(bsl, "data/liam_test_data/SL_gps_30min_LGH.rds")
+lapply(dir('R', '*.R', full.names = TRUE), source)
+bsl<-readRDS("data/liam_test_data/SL_gps_30min_LGH.rds")
+
+# reset gps names from start
 setnames(gps, c("x", "y", "t"), c("x_proj", "y_proj", "datetime")) 
-# merge(gps, bsl, by="deploy_id", all.x=T)
 
 #get date range for eacvh week and deployid in gps
 gps[, mindate := min(idate), by=.(week, deploy_id)]
@@ -79,21 +82,8 @@ bsl[, date:=tstrsplit(date, " ", keep = 1)]
 bsl[, date:=lubridate::ymd(date)]
 bsl[, idate:=as.IDate(date)]
 
-did<- ref[, unique(deploy_id)]
-d="24653_2018-12-17"
-for (d in did) {
-    r1<-ref[deploy_id==d]
-    
-  bsl[deploy_id == d, mindate:= r1$mindate]
-}
-
 a<-merge(ref, bsl, by="deploy_id", allow.cartesian=T, all=T)
-t<-a[, .N, deploy_id]
-t1<-bsl[, .N, deploy_id]
-#test if fixes per deployID are same after merge
-m1<-merge(t, t1, by="deploy_id", all=T) #they arent???? but does 'a' always have mroe
-m1[, diff:=N.x-N.y] #yes merged one is always more
-#this is because one deployid has multiple weeks, so try to get week
+#a always has more  because one deployid has multiple weeks, so try to get week
 a[idate >= mindate & idate <= maxdate, week.merge := week]
 wbsl<-a[!is.na(week.merge)]
 #testagain
@@ -118,7 +108,19 @@ wbsl[, n.hare.weeks := uniqueN(week), by = deploy_id]
 nw <- wbsl[, unique(n.hare.weeks), by = deploy_id]
 nw[, sum(V1)] #586 hare weeks
 
-#we have 7 days, but want arbitrarily at least 85% of possible steps
+wbsl[, weekdate := mean(unique(idate)), by = .(deploy_id, week)]
+
+#check that ha weeks in sl data and gps data are the same
+# t<-wbsl[, unique(weekdate), deploy_id]
+# t1<-gps[, unique(weekdate), deploy_id]
+# m1<-merge(t, t1, by=c("deploy_id","V1"), all.x=T)
+# wbsl[deploy_id=="23717_2017-12-13", unique(weekdate)]
+# gps[deploy_id=="23717_2017-12-13", unique(weekdate)] 
+# gps[deploy_id=="23717_2017-12-13", unique(idate), week] 
+#this is different bc Jules did the mean on actual data, so not always the actual center point of week
+
+
+#we have 7 days, but want a certain % of possible steps
 wbsl[, n.steps:=.N, .(deploy_id, week)] # n steps
 wbsl[, prop.steps := n.steps/((48*7)-1)]  #max fixes are 48 per day x 7 days, but minus 1 for n steps
 wbsl[, hist(prop.steps)]
@@ -126,20 +128,22 @@ wbsl[sl_>22500] #nothing unrealistic 45kph=45000mph=22500m/30mins
 
 #lets calc mean movement  per day before we clean based on prop.steps
 
-mpd<-wbsl[, sum(sl_), .(idate, deploy_id, week, prop.steps, n.steps)]
+mpd<-wbsl[, sum(sl_), .(idate, deploy_id, week, weekdate, prop.steps, n.steps)]
 setnames(mpd, "V1", "meter_day")
 #avg meter per day per week
-ampd<-mpd[, median(meter_day), .(deploy_id, week, prop.steps, n.steps)]
+ampd<-mpd[, median(meter_day), .(deploy_id, week, weekdate, prop.steps, n.steps)]
 setnames(ampd, "V1", "avg_meter_day")
 #overall avg
 ampd[, median(avg_meter_day)] 
 
+
 ggplot(ampd)+
-  geom_point(aes(x=prop.steps, y=avg_meter_day)) +
-  coord_cartesian(ylim=c(0,9000))
+  geom_point(aes(x=prop.steps, y=avg_meter_day)) #+
+  # coord_cartesian(ylim=c(0,9000))
 
 #lets just arbiitrailry remove below .5 success for now (could do sensitivty analyssi later)
-cl.am<-ampd[prop.steps>=0]
+cl.am<-ampd[prop.steps>=.5] #REMEMBER THAT THIS %FIXES DOESNT REPRESETN OUR ACTUAL DATA
+#WE CANT USE SINGLE FIXES BC NO STEP, AND CANT COMPARE DISTANCES OVER VARIABLE TIMES aSSUMING STRAIGHT LINE MOVEMENT
 cl.am[, hist(avg_meter_day)]
 
 
@@ -150,15 +154,69 @@ hrm<-merge(cl.am, sub.dat, by=c("deploy_id", "week"), all.x=T)
 #remove buns with no HRs
 hrm<-hrm[!is.na(M90)]
 
+#melt for better plotting
+mhr<-melt(hrm, na.rm = FALSE, measure.vars = c("M90", "M75", "M50"),
+          variable.name = "MCP_percentile", value.name="MCP_area")
+
+
 #plot areas against fix rate
-ggplot(hrm) +
-  geom_jitter(aes(x= avg_meter_day, y = M90), colour="red", width = 0.5) +
-  geom_jitter(aes(x= avg_meter_day, y = M75), colour="green", width = 0.5) +
-  geom_jitter(aes(x= avg_meter_day, y = M50), colour="blue", width = 0.5) +
-  coord_cartesian(xlim=c(0,2500), ylim=c(0,20)) # zoom in on bulk of data
+# ggplot(hrm) +
+#   geom_jitter(aes(x= avg_meter_day, y = M90), colour="red", width = 0.5) +
+#   geom_smooth(aes(x= avg_meter_day, y = M90), colour="red", method='lm', formula= y~x) +
+#   geom_jitter(aes(x= avg_meter_day, y = M75), colour="green", width = 0.5) +
+#   geom_smooth(aes(x= avg_meter_day, y = M75), colour="green", method='lm', formula= y~x) +
+#   geom_jitter(aes(x= avg_meter_day, y = M50), colour="blue", width = 0.5) +
+#   geom_smooth(aes(x= avg_meter_day, y = M50), colour="blue", method='lm', formula= y~x) 
+
+noz<-ggplot(mhr, aes(x=avg_meter_day, y=MCP_area, colour=MCP_percentile)) +
+    geom_jitter() +
+    geom_smooth(method='lm', formula= y~x) +
+  labs(x="Weekly Avg Movement Rate (meters/day)", y="MCP Area (Ha)", 
+       title="30 min fixes, minimum of 50% fix success per week")
+
+# submhr<-  mhr[avg_meter_day<10000]
+# submhr[MCP_area>20] #The HR outlier is less likely due to daily movements and more likely shifted areas
 
 
+wz<-ggplot(mhr, aes(x=avg_meter_day, y=MCP_area, colour=MCP_percentile)) +
+    geom_jitter() +
+    geom_smooth(method='lm', formula= y~x) +
+  labs(x="Weekly Avg Movement Rate (meters/day)", y="MCP Area (Ha)",
+       title="Same as left figure, but zoomed in on bulk of data") + 
+  coord_cartesian(xlim=c(0,2000), ylim=c(0,15)) # zoom in on bulk of data
 
+(HRmoveplots <- ggarrange(noz, wz, ncol = 2, nrow = 1))
+
+
+ggsave("output/figures/HR_dailymovement.jpeg", HRmoveplots, width = 17, height = 9, units = "in")
+
+  
+  
+  
+  
+  
+#Outliers seems to be an individual that did large DAILY movements back and forth betwn two areas
+  
+  #look into ouitliers by redoing plots by id 
+  ##UTM PROJECTION DOESNT SEEM TO BE IN RIGHT LOCATION (POINTS ARE IN ALASKA)?!?!
+  cl.am[avg_meter_day>10000]
+  ampd[deploy_id=="23717_2017-12-13"]
+  
+
+  gpsdf<-as.data.frame(gps)
+  
+  p<-ggplot(gpsdf, aes(x=x_proj, y=y_proj)) +
+    geom_point(aes(size=0.1, colour= idate))
+  
+  plots = gpsdf %>%
+    group_by(deploy_id) %>%
+    do(plots = p %+% . + facet_wrap(~deploy_id + weekdate))
+  plots$plots[[91]]
+  
+  pdf("output/figures/GPSfixes_each_weekly_HR.pdf",
+      width = 10, height = 7)
+  plots$plots
+  dev.off()
 
 
 
