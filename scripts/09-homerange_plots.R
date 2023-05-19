5a10271b6cb0fd9d2cba9165d9f8072f7d3e30bc
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-05-19 14:29:19 -0600

    hr by year after removing food adds

--- scripts/09-homerange_plots.R
+++ scripts/09-homerange_plots.R
@@ -16,67 +16,36 @@ DT <- DT[!M90 > 20]
 #reorder phase cycles
 DT[, phase := factor(phase, levels = c("increase", "peak", "decrease", "low"))]
 
-#rename some variables for figures
+#rename sex categories
 DT[Sex == 1, Sex := "Male"][Sex == 2, Sex := "Female"]
+#rename food categories
 DT[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
+
+#create 2nd data frame that has no food adds
+DTnofood <- DT[!Food == "Food add"]
 
 #set colors for cycle phases
 cols <- c("increase" = "purple", "peak" = "green4", decrease = "orange", low = "red3")
 
 
-
-# By year only ------------------------------------------------------------
+# By year and phase for non-food add data ------------------------------------------------------------
 
 (byyear <- 
-   ggplot(DT)+
+   ggplot(DTnofood)+
    geom_boxplot(aes(x = winter, y = M90, color = phase))+
    labs(y = "90% MCP area (ha)", x = "Winter")+
    scale_color_manual(values = cols)+
    theme_boxplots+
    theme(axis.text.x.bottom = element_text(size = 8)))
 
-
-
-# Home range in response to resource parameters --------------------------------------------------------
-
-(byfood <-
-   ggplot(DT[!is.na(Sex)])+
-   geom_boxplot(aes(x = Sex, y = M90, color = Food))+
-   labs(y = "90% MCP area (ha)", x = "Sex")+
-   theme_boxplots)
-
-(bysnow <- 
-  ggplot(DT)+
-  geom_point(aes(x = SD, y = M90, color = phase))+
-  labs(y = "90% MCP area (ha)", x = "Snow depth (cm)")+
-  theme_densities)
-
-(bymass <- 
-  ggplot(DT)+
-  geom_point(aes(x = Weight, y = M90, color = phase))+
-  labs(y = "90% MCP area (ha)", x = "Body mass (g)")+
-  theme_densities)
-
-(hrresource <- ggarrange(byfood, bysnow, bymass, ncol = 1, nrow = 3))
-
-
-# Distribution of home range sizes by year
-(hr_distrn <- ggplot(incl.out, aes(x = M90)) + 
-    geom_histogram(binwidth=1, color="black", fill="grey") +
-    labs(x="90% MCP area (Ha) - bins are 1 Ha each", y="Count") + 
-    facet_wrap(~phase))
-    
+#haven't saved this yet but it's an option
+(byphase <- 
+    ggplot(DTnofood)+
+    geom_boxplot(aes(x = phase, y = M90))+
+    labs(y = "", x = "Cycle phase")+
+    theme_boxplots)
 
 
 
-
-
-# save figures ------------------------------------------------------------
-
-
-ggsave("output/figures/HRbyresource.jpeg", hrresource, width = 5, height = 8, units = "in")
-
-ggsave("Output/figures/HRbyyear.jpeg", byyear, width = 6, height = 4, units = "in")
-
-ggsave("Output/figures/HR_distribution.jpeg", hr_distrn, width = 11, height = 8.5, units = "in")
+ggsave("Output/figures/HRbyyear.jpeg", byyear, width = 7, height = 6, units = "in")
 
