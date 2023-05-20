500c3ce18c15cb11034491df45137fa3b6adc46c
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-05-19 20:47:44 -0600

    overhaul results script. Now just one model with treatment years only, and one model for all years but excluding treatment

--- scripts/10-final_models_figures.R
+++ scripts/10-final_models_figures.R
@@ -14,58 +14,119 @@ dat[, phase := factor(phase, levels = c("increase", "peak", "decrease", "low"))]
 #convert weight to kg for ease of coefficient rounding
 dat[, Weight := Weight/1000]
 
-#read in snow for markdown doc
-snow <- readRDS("data/snowgrids.rds")
-#remove winter with no collar data from snow data
-snow <- snow[!winter == "2014-2015" & !winter == "2021-2022"]
+#rename food categories
+dat[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
+
+#make a dataframe with only control hares. This will be all years
+nofood <- dat[Food == "Control"]
+
+#pull out the years with food add
+foodyears <- dat[Food == "Food add", unique(winter)]
+
+#make a dataframe to only include the winters with food add 
+yesfood <- dat[winter %in% foodyears]
 
 
-# home range covariate correlation test -----------------------------------
 
-cor(dat$M90, dat$M75)
-cor(dat$M75, dat$M50)
-cor(dat$M50, dat$M90)
 
 # covariate correlation test --------------------------------------------------------
 
 #subset data to only variables that we need to test co linearity on (numeric only)
-forcor <- dat[, .(haredensity, mortrate, Weight, SD)]
+forcor <- nofood[, .(haredensity, mortrate, weekwinterday)]
 
 #run correlation, look at matrix style output
 round(cor(forcor, use = "complete.obs"), digits = 2)
 
 
 
-# AIC for analysis across winters -----------------------------------------
+# Create models -----------------------------------------------------------
 
-#I am thinking we run three models with all variables, split by the demographic variable
-#this tests each prediction across all winters, shows which is the most explanatory 
-# not an AIC
+# NO FOOD mixed model for mort rate and hare density
+NFmixed <- lmer(M90 ~ mortrate + haredensity + (1|id), data = nofood)
 
-#build models
-phase_res <- lm(M90 ~ phase + Weight*Food + SD, dat)
-pred_res <- lm(M90 ~ mortrate + Weight*Food + SD, dat)
-comp_res <- lm(M90 ~ haredensity + Weight*Food + SD, dat)
-null <- lm(M90 ~ 1, dat)
+# NO FOOD linear model for mort rate and hare density
+NFlinear <- lm(M90 ~ mortrate + haredensity , data = nofood)
+
+# YES FOOD linear model for mort rate and hare density
+WFlinear <- lm(M90 ~ mortrate*Food + haredensity*Food , data = yesfood)
+
+#to get effects for the coat colour in the energetics model
+effsP <- ggpredict(WFlinear, terms = c("mortrate", "Food"))
+
+effsD <- ggpredict(WFlinear, terms = c("haredensity", "Food"))
+
+
+# Figures with NO FOOD ------------------------------------------------------------------
+
+#these are coefficients from the No food linear model
+NFd <- coef(NFlinear)["haredensity"]
+NFp <- coef(NFlinear)["mortrate"]
+NFint <- coef(NFlinear)["(Intercept)"]
+
+(NFdensity <- 
+   ggplot(nofood)+
+   geom_point(aes(x = haredensity, y = M90))+
+   labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
+   theme_densities)
+
+(NFmort <- 
+    ggplot(nofood)+
+    geom_point(aes(x = mortrate, y = M90))+
+    geom_abline(aes(intercept = NFint, slope = NFp))+
+    labs(y = "", x = "Mortality rate")+
+    theme_densities)
+
+    
+(hrNOFOOD <- ggarrange(NFdensity, NFmort, 
+                      ncol = 2, nrow = 1))
+
+
+
+
+# Figures from food add years  --------------------------------------------
+
+foodcols <- c("Food add" = "blue3", "Control" = "grey40")
+
+#these are coefficients from the No food linear model
+WFd <- coef(WFlinear)["haredensity"]
+WFp <- coef(WFlinear)["mortrate"]
+WFint <- coef(WFlinear)["(Intercept)"]
+
+
+(WFdensity <- 
+   ggplot()+
+    geom_point(aes(x = haredensity, y = M90, color = Food), data = yesfood)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, group = group, fill = group),
+              colour = "grey80", alpha = .3, data = effsD)+
+    geom_line(aes(x = x, y = predicted, group = group, color = group),
+              size = 1, data = effsD)+
+    scale_color_manual(values = foodcols, guide = NULL)+
+    scale_fill_manual(values = foodcols, guide = NULL)+
+    labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
+    theme_densities)
+
+
+(WFmort <- 
+    ggplot()+
+    geom_point(aes(x = mortrate, y = M90, color = Food), data = yesfood)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, group = group, fill = group),
+            colour = "grey80", alpha = .3, data = effsP)+
+    geom_line(aes(x = x, y = predicted, group = group, color = group),
+            size = 1, data = effsP)+
+    scale_color_manual(values = foodcols, guide = NULL)+
+    scale_fill_manual(values = foodcols)+
+    labs(y = "", x = "Mortality rate")+
+    theme_densities)
+
+(hrYESFOOD <- ggarrange(WFdensity, WFmort, ncol = 2, nrow = 1))
+
+
+
+# Create model outputs ----------------------------------------------------
 
 #list models and provide names
-mods <- list(phase_res, pred_res, comp_res, null)
-names <- c("Phase and resource", "Predation and resource", "Competition and resource", "Null")
-
-#create AIC table on list of models
-AIC<-as.data.table(aictab(REML = F, cand.set = mods, modnames = names, sort = TRUE))
-AIC[, ModelLik := NULL]
-AIC[, Cum.Wt := NULL]
-AICcycle <- AIC %>% mutate_if(is.numeric, round, digits=3) #round whole table to 3 digits
-
-#which models are less than 2 delta AIC? 
-topmods <- AICcycle[Delta_AICc < 2, return((Modnames))]
-
-
-
-
-
-# Table with coef outputs -------------------------------------------------
+mods <- list(NFlinear, WFlinear)
+names <- c("Without treatment", "With treatment")
 
 
 #apply the lm_out function to the top to same list of models as in AIC
@@ -75,53 +136,18 @@ outall$Model <- names
 outall[, `(Intercept)` := NULL]
 
 outall <- outall[!Model == "Null"]
-outall[, V1 := NULL]
 
 
 
-setcolorder(outall, c("Model", "phasepeak", "phasedecrease", "phaselow", "haredensity", "mortrate",
-                      "Weight", "Food1", "Weight:Food1", "SD", "rsq"))
+setcolorder(outall, c("Model", "haredensity", "mortrate", "FoodControl", 
+                      "FoodControl:haredensity", "mortrate:FoodControl", 
+                      "rsq"))
 
-names(outall) <- c("Model", "Phase peak", "Phase decrease", "Phase low", "Hare density", "Mortality rate", 
-                   "Weight", "Food add", "Weight*Food", "Snow depth", "R2")
+names(outall) <- c("Model", "Density", "Mortality", "Treatment",
+                   "Treatment*Density", "Treatment*Mortality",
+                   "R2")
 
 
-
-# Figure ------------------------------------------------------------------
-
-#these are showing trends from the comp_res models
-d <- coef(comp_res)["haredensity"]
-int_d <- coef(comp_res)["(Intercept)"]
-sd_d <- coef(comp_res)["SD"]
-
-#trends from the pred_res model
-p <- coef(pred_res)["mortrate"]
-int_p <- coef(pred_res)["(Intercept)"]
-
-(byhdensity <- 
-   ggplot(dat)+
-   geom_point(aes(x = haredensity, y = M90))+
-   geom_abline(aes(intercept = int_d, slope = d))+ 
-   labs(y = "90% MCP area (ha)", x = "Hare Density (hares per ha)")+
-   theme_densities)
-
-(bysnow <- 
-    ggplot(dat)+
-    geom_point(aes(x = SD, y = M90))+
-    geom_abline(aes(intercept = int_d, slope = sd_d))+
-    labs(y = "", x = "Snow depth (cm)")+
-    theme_densities)
-
-(bymort <- 
-    ggplot(dat)+
-    geom_point(aes(x = mortrate, y = M90))+
-    geom_abline(aes(intercept = int_p, slope = p))+
-    labs(y = "90% MCP area (ha)", x = "Mortality rate")+
-    theme_densities)
-
-    
-(hrcrosscycle <- ggarrange(byhdensity, bysnow, bymort, 
-                      ncol = 2, nrow = 2))
 
 
 
@@ -130,8 +156,9 @@ int_p <- coef(pred_res)["(Intercept)"]
 
 
 
-ggsave("output/figures/HRcrosscycle.jpeg", hrcrosscycle, width = 12, height = 11, units = "in")
+ggsave("output/figures/HRnofood.jpeg", hrNOFOOD, width = 12, height = 6, units = "in")
 
-fwrite(AIC, "Output/results/AIC_cross_cycle.csv")
+ggsave("output/figures/HRwithfood.jpeg", hrYESFOOD, width = 12, height = 6, units = "in")
 
-fwrite(outall, "Output/results/models_cross_cycle.csv")
+
+fwrite(outall, "Output/results/model_outputs.csv")
