ebee0f3057637075aca2adc553b0807ec9fcf049
Juliana Balluffi-Fry <jballuffi@gmail.com>
2023-06-19 16:47:37 -0600

    change figure to show data in more detail

--- scripts/09-homerange_plots.R
+++ scripts/09-homerange_plots.R
@@ -22,30 +22,30 @@ DT[Sex == 1, Sex := "Male"][Sex == 2, Sex := "Female"]
 #rename food categories
 DT[Food == 1, Food := "Food add"][Food == 0, Food := "Control"]
 
-#create 2nd data frame that has no food adds
-DTnofood <- DT[Food == "Control"]
-
 #set colors for cycle phases
 cols <- c("increase" = "purple", "peak" = "green4", "decrease" = "orange", "low" = "red3")
+foodcols <- c("Food add" = "red3", "Control" = "grey30")
 
-#pull out the years with food add
-foodyears <- DT[Food == "Food add", unique(winter)]
-
-#make a data frame to only include the winters with food add 
-yesfood <- DT[winter %in% foodyears]
 
 
 
 # Density over time --------------------------------------------------------
 
+(h <- ggplot(densities)+
+  geom_path(aes(x = date, y = haredensity, group = winter, color = phase))+
+  scale_color_manual(values = cols)+
+  labs(x = "", y = "Hares per ha")+
+  theme_boxplots+
+  theme(axis.text.x.bottom = element_text(size = 8)))
+
+#other version
 #pull means by year
 wintermeans <- densities[, .(mean(haredensity), mean(mortrate, na.rm = TRUE), phase), by = winter]
 names(wintermeans) <- c("winter", "haredensity", "mortrate", "phase")
 #remove winter with no collar data
 wintermeans <- wintermeans[!winter == "2021-2022"]
 
-#hare density over time
-(h <- ggplot(wintermeans)+
+(ggplot(wintermeans)+
     geom_path(aes(x = winter, y = haredensity, group = 1))+
     geom_point(aes(x = winter, y = haredensity, color = phase), size = 2)+
     scale_color_manual(values = cols)+
@@ -57,8 +57,24 @@ wintermeans <- wintermeans[!winter == "2021-2022"]
 
 # mortality rate over time ------------------------------------------------
 
-#mort rate over time
-(l <- ggplot(wintermeans)+
+#cut to just the first day of any month
+minmonths <- densities[, ymd(min(date)), by = .(mnth, winter)]
+monthdates <- as.list(minmonths$V1)
+#subset to new table
+morts <- densities[date %in% minmonths$V1]
+#remove NAs 
+morts <- morts[!is.na(mortrate)]
+setorder(morts, date)
+
+(l <- ggplot(morts)+
+  geom_path(aes(x = date, y = mortrate, group = winter, color = phase))+
+  scale_color_manual(values = cols)+
+  labs(x = "", y = "Probability of mortality")+
+  theme_boxplots+
+  theme(axis.text.x.bottom = element_text(size = 8)))
+
+#other version
+(ggplot(wintermeans)+
     geom_path(aes(x = winter, y = mortrate, group = 1))+
     geom_point(aes(x = winter, y = mortrate, color = phase), size = 2)+
     scale_color_manual(values = cols)+
@@ -71,23 +87,12 @@ wintermeans <- wintermeans[!winter == "2021-2022"]
 # By year and phase for non-food add data ------------------------------------------------------------
 
 (byyear <- 
-   ggplot(DTnofood)+
-   geom_boxplot(aes(x = winter, y = M90, color = phase))+
+   ggplot(DT)+
+   geom_boxplot(aes(x = winter, y = M90, color = Food))+
    labs(y = "90% MCP area (ha)", x = "Winter")+
-   scale_color_manual(values = cols)+
+   scale_color_manual(values = foodcols)+
    theme_boxplots+
    theme(axis.text.x.bottom = element_text(size = 8)))
-
-
-
-# Home range in response to food add --------------------------------------
-
-(bytreatment <- 
-  ggplot(yesfood)+
-  geom_boxplot(aes(x = Food, y = M90))+
-  labs(y = "90% MCP area (ha)", x = "Food treatment")+
-  theme_boxplots)
-
 
 
 
@@ -97,4 +102,3 @@ fullbyyear <- ggarrange(h, l, byyear, ncol = 1, nrow = 3)
 
 
 ggsave("Output/figures/sumfigure.jpeg", fullbyyear, width = 6, height = 9, units = "in")
-ggsave("Output/figures/HRbytreatment.jpeg", bytreatment, width = 4, height = 5, units = "in")
